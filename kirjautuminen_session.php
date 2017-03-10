42ae8f96008ca6cf5fc0f75f83e1632cb584b11c
annivigren <anni.vigren@gmail.com>
2017-03-10 14:14:47 +0200

    Toimiva kirjautumissivu

--- kirjautuminen_session.php
+++ kirjautuminen_session.php
@@ -2,35 +2,31 @@
 <html>
 <body>
 
+
+  <?php
+  //Tee kirjautumisjärjestelmä ilman tietokantaa.
+  //– Sivustolle jolla on vähintään 2 eri sivua.
+  //– Kysyy tunnusta ja muistaa kirjautumisen ettei kysy
+  //uudestaan siirryttäessä sivulta toiselle.
+  //– Ei päästä millekään sivulle ellei ole oikeaa salasanaa.
+
+  //aloitetaan istunto
+
+  session_start();
+
+  //määritetään istuntomuuttujat
+
+  $_SESSION["nimi"] = $_POST["nimi"];
+  $_SESSION["salasana"] = $_POST["salasana"];
+  ?>
+
 <?php
-
-//Tee kirjautumisjärjestelmä ilman tietokantaa.
-//– Sivustolle jolla on vähintään 2 eri sivua.
-//– Kysyy tunnusta ja muistaa kirjautumisen ettei kysy
-//uudestaan siirryttäessä sivulta toiselle.
-//– Ei päästä millekään sivulle ellei ole oikeaa salasanaa.
-
-//aloitetaan istunto
-
-session_start();
-
-//määritetään istuntomuuttujat
-
-$_SESSION["nimi"] = $_POST["nimi"];
-$_SESSION["salasana"] = $_POST["salasana"];
-
-if (!empty($_POST['nimi'])) {
-
-echo "Tervetuloa! ".($_session['nimi']);
-}
-else{
-
-echo "Kirjoita nimi ja salasana";
-}
-
-
- ?>
-
+//Kun nimi kenttä on tyhjä pyydetään kirjoittamaan nimi ja salasana
+if (empty($_POST['nimi']))
+ {echo "Kirjoita nimi ja oikea salasana";
+?>
+<br>
+ <br>
  Nimi:
      <br>
        <form action="kirjautuminen_session.php" method="post">
@@ -41,6 +37,35 @@ echo "Kirjoita nimi ja salasana";
            <br>
            <input type="submit" name="Kirjaudu" value="Kirjaudu."/>
        </form>
+<?php
+//Jos salasanaa ei ole kirjoitettu oiken, pyydetään täyttämään tiedot oikein
+elseif ($_POST['salasana'] != "123")
+  {echo "Salasana väärin. Yritä uudelleen";
+?>
+    <br>
+     <br>
+     Nimi:
+         <br>
+           <form action="kirjautuminen_session.php" method="post">
+             <input type="string" name="nimi" required/><br>
+     Salasana:
+             <br>
+             <input type="string" name="salasana" required/><br>
+               <br>
+               <input type="submit" name="Kirjaudu" value="Kirjaudu."/>
+           </form>
 
+<?php
+//Kun tiedot on kirjoitettu oikein, voidaan niitä tarkastella toisella sivulla.
+} else{
+  echo "Tervetuloa ".($_POST['nimi']." !");
+  echo "<br />";
+  echo "<br />";
+  echo "Tarkastele tietojasi";
+
+
+}
+
+  ?>
 </body>
 </html>
